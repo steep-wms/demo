cd553103de054021621a0babf96c58d033478221
christoph-werner <64007244+christoph-werner@users.noreply.github.com>
2020-11-22 17:04:33 +0100

    return server response to user, hardened fetch against errors

--- src/pages/playground.js
+++ src/pages/playground.js
@@ -1,5 +1,6 @@
 import Head from "next/head"
 import styles from "../styles/Home.module.css"
+import { useRef } from "react"
 
 // example workflow
 const data = {
@@ -19,6 +20,25 @@ const data = {
 }
 
 export default function Playground() {
+
+  const ref = useRef()
+
+  // sends the example workflow on button-click
+  const handleClick = async () => {
+    // send JSON fetch request and wait for reply
+    const reply = await postJSON(data)
+    // console.log(reply)
+    // reply is positive
+    if (reply[0] === 1) {
+      let status = "Workflow "+reply[1].status+" with ID "+reply[1].id
+      ref.current.innerHTML = status
+    }
+    // bad reply
+    else {
+      ref.current.innerHTML = reply[1].error
+    }
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -40,6 +60,7 @@ export default function Playground() {
         </p>
 
         <button onClick={handleClick} className="btn btn-primary">Execute the workflow!</button>
+        <p ref={ref}></p>
       </main>
 
       <footer className={styles.footer}>
@@ -56,30 +77,31 @@ export default function Playground() {
   )
 }
 
-// sends the example workflow on button-click
-const handleClick = (e) => {
-  // console.log(e)
-  postJSON(data)
-}
-
 /**
  * POSTs the given JSON payload "data" to the Steep WMS server.
  * The server needs to run with "-e STEEP_HTTP_CORS_ENABLE=true" (docker) or "steep.​http.​cors.​enable=true" (config).
  * @param {JSON} data
+ * @returns {{int}, {JSON}} int is either 1 for succesful fetch or -1 for error. payload/error-message is in the JSON
  */
-function postJSON(data) {
-  fetch("http://localhost:8080/workflows", {
+async function postJSON(data) {
+  const response = await fetch("http://localhost:8080/workflows", {
     method: "POST",
     mode: "cors",
     // Steep doesnt accept any headers by default
     body: JSON.stringify(data)
     })
-    .then(response => {
-      console.log(response)
-      if (!response.ok) {
-        throw new Error("Network response was not ok")
+    .then(async res => {
+      // response from server is OK
+      if (res.ok) {
+        const reply = await res.json()
+        console.log(reply)
+        return [1, reply]
+      // bad request
       }
-      return response.blob()
-      })
+      else throw Error("Request rejected: "+res.status)
+    })
+    // also catch HTTP errors
+    .catch(err => {return [-1, { error:"Caught error: "+err+"." }]})
+  return response
 }
 
